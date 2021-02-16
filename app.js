b7756112455ce669a662650fdac0ff61b6785ba8
DishenMakwana <dishenmakwana.dm@gmail.com>
2021-02-16 12:46:24 +0530

    Add changes

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ var app = express();
 var mongoose = require('mongoose');
 var mongoDB =
   'mongodb+srv://dishen:dishen@cluster0.zuylw.mongodb.net/local_library?retryWrites=true&w=majority';
+
+// Set up mongoose connection
+var dev_db_url =
+  'mongodb+srv://dishen:dishen@cluster0.zuylw.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
